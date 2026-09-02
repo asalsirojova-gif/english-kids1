325d583bdb4c5b6263883c514f44c9d837fd8331
asalsirojova-gif <asalsirojova@gmail.com>
2026-09-02 23:48:29 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,33 @@
-const CACHE="arabic-kids-v1";const A=["./","./index.html","./style.css","./script.js","./manifest.json","./icon.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let c=x.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return x}).catch(()=>caches.match("./")))));
+const CACHE = "arabic-kids-v1";
+const A = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon.svg"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(A))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => {
+      return r || fetch(e.request).then(x => {
+        let c = x.clone();
+        caches.open(CACHE).then(k => k.put(e.request, c));
+        return x;
+      });
+    }).catch(() => caches.match("./"))
+  );
+});
